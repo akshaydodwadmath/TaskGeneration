e752a9e70d0eeceaa435c27dc4144d4a66e05a34
Akshay Dodwadmath <adodwadm@wks-22-0148.mpi-sws.org>
2022-11-01 14:40:57 +0100

    Random generator updated with extra semantic information about code structure

--- Baseline/random_generator/random_generator.py
+++ Baseline/random_generator/random_generator.py
@@ -3,6 +3,7 @@ import math
 import argparse
 import json
 import random
+import os
 
 from karel.consistency import Simulator
 
@@ -16,13 +17,13 @@ actions = [
 ]
 
 commands = [
-            ['REPEAT','r(','r)'],
-            ['WHILE','w(','w)'],
-            ['IF','i(','i)']
+            ['REPEAT','r(','r)', 0],
+            ['WHILE','w(','w)', 1],
+            ['IF','i(','i)', 1]
             ]
 command_if_else = [
-            ['IFELSE','i(','i)'],
-            ['ELSE','e(','e)']
+            ['IFELSE','i(','i)', 1],
+            ['ELSE','e(','e)', 1]
             ]
 
 cond = [
@@ -39,45 +40,134 @@ cond = [
     ['R=']# repeat options 2 to 10
     ]
 
-required_ctypes = [#['action1'],
+required_ctypes = [['action'], #Type01
                    
-                   #['action1', 'ctrl1', 'cond', 'copen1', 'action2', 'cclose1', 'action3'] ], 
-                   #['action1', 'cif1', 'cond', 'c_ifopen1', 'action2', 'c_ifclose1',
-                   #             'celse1', 'c_elseopen1', 'action3', 'c_elseclose1', 'action4'] ], 
+                   ['action', 
+                    'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                    'action' ], #Type02
                    
-                    #['action1', 'ctrl1', 'cond', 'copen1', 'action2', 'cclose1', 'action3',
-                    # 'action4', 'ctrl2', 'cond', 'copen2', 'action5', 'cclose2', 'action6'],
-                    #]
-                    ['action1', 'cif1', 'cond', 'c_ifopen1', 'action2', 'c_ifclose1',
-                                'celse1', 'c_elseopen1', 'action3', 'c_elseclose1', 'action4',
-                    'action5', 'ctrl1', 'cond', 'copen1', 'action6', 'cclose1', 'action7'] ]
-                   #'(D_IF()D_ELSE()D_CTRL())',
-                   #'(D_CTRL()D_IF()D_ELSE())',
-                   #'(D_IF()D_ELSE()D_IF()D_ELSE())',
+                   ['action', 
+                    'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                    'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                    'action'], #Type03
+                   ['action', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1',
+                     'action', 
+                     'ctrl2', 'cond2', 'copen2', 'action', 'cclose2', 
+                     'action'],#Type04
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                    'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                    'action'],#Type05
+                    ['action', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1',
+                     'action', 
+                    'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                    'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                    'action'],#Type06
+                    ['action', 
+                    'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                    'celse', 'c_elseopen', 'action', 'c_elseclose',  
+                    'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                    'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                    'action'],#Type07
                    
-                   #'(D_CTRL(D_CTRL()))',
-                   #'(D_CTRL(D_IF()D_ELSE()))',
-                   #'(D_IF(D_CTRL())D_ELSE())',
-                   #'(D_IF(D_IF()D_ELSE())D_ELSE())',
-                   #'(D_IF()D_ELSE(D_CTRL()))',
-                   #'(D_IF()D_ELSE(D_IF()D_ELSE()))',
+                    ['action', 
+                     'ctrl1', 'cond1', 'copen1', 
+                     'ctrl2', 'cond2', 'copen2', 'action', 'cclose2', 
+                     'action', 'cclose1', 
+                     'action'], #Type 08
+                    ['action', 
+                     'ctrl1', 'cond1', 'copen1', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action', 'cclose1',
+                     'action'],
+                    ['action', #Type 09
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 10
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 11
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 12
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 13
+                    
+                    
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'ctrl2', 'cond2', 'copen2', 'action', 'cclose2', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 14
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 15
+                   
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'ctrl1', 'cond1', 'copen1', 'action', 'cclose1', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action', 'c_elseclose', 
+                     'action'], #Type 16
+                    ['action', 
+                     'cif', 'c_cndif', 'c_ifopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action','c_ifclose',
+                     'celse', 'c_elseopen', 
+                     'cif', 'c_cndif', 'c_ifopen', 'action', 'c_ifclose',
+                     'celse', 'c_elseopen', 'action', 'c_elseclose', 
+                     'action', 'c_elseclose', 
+                     'action']] #Type 17
+    
+def add_args(parser):
+    parse_group = parser.add_argument_group("Conversion",
+                                        description="Conversion options")
+    parse_group.add_argument('--data_dir', type=str, default='data')
+    
+    
 
-                   #'(D_IF(D_CTRL())D_ELSE(D_CTRL()))',
-                   #'(D_IF(D_IF()D_ELSE())D_ELSE(D_CTRL()))',
-                   #'(D_IF(D_CTRL())D_ELSE(D_IF()D_ELSE()))',
-                   #'(D_IF(D_IF()D_ELSE())D_ELSE(D_IF()D_ELSE()))']
-                   #"CodeType": "FeatureVector"
-                   
-                   #'( D_CTRL ( ) D_CTRL ( ) D_CTRL ( ) )',
-                   #'( D_CTRL ( ) D_CTRL ( D_CTRL ( ) ) )',
-                   #'( D_CTRL ( ) D_CTRL ( D_CTRL ( D_CTRL ( ) ) ) )',
-                   #'( D_CTRL ( ) D_CTRL ( D_IF ( ) D_ELSE ( ) ) )',
-                   #'( D_CTRL ( ) D_IF ( ) D_ELSE ( ) )',
-                   #'( D_CTRL ( ) D_IF ( D_CTRL ( ) ) D_ELSE ( ) )',
-                   #'( D_CTRL ( D_CTRL ( ) ) )',
-                   #'( D_CTRL ( D_CTRL ( ) ) D_CTRL ( ) )',
-                   #'( D_CTRL ( D_CTRL ( ) ) D_CTRL ( ) D_CTRL ( ) )'
-                   
+parser = argparse.ArgumentParser(
+    description='Convert code to code types.')    
+add_args(parser)
+args = parser.parse_args()
+
+text_path = os.path.join(args.data_dir, "{}.txt".format('train'))
+text = ""
+
 token_beg = ['DEF', 'run', 'm(']
 token_end = ['m)' ] 
 max_actions = [i for i in range(2,17)]
@@ -91,24 +181,12 @@ def generate_codes(code_type, max_nb_actions):
     action_in_code_type = []
     ctrl_count = 0
     cifelse_count = 0
-    
     for token in code_type:
-        if('cif' in token):
-            code.append(token)
-            cifelse_count += 1
-        elif('ctrl' in token):
-            code.append(token)
+        if('ctrl' in token):
             ctrl_count += 1
-        elif(token == 'cond'):
-            rand_cond = random.choice(cond)
-            if(rand_cond == cond[-1]):# for repeat
-                rand_cond = [str(rand_cond[0]) + str(random.choice(max_repeat))]
-            code += rand_cond
-        else:
-            code.append(token)
-    
+        code.append(token)
     ctrl_set = random.choices(commands, k=ctrl_count)
-    
+
     #Add actions
     while(action_set):
         index = 0
@@ -123,10 +201,10 @@ def generate_codes(code_type, max_nb_actions):
                     break
             index+=1
     
-    
     #Add control statements and brackets
     open_set = []
     close_set = []
+    cond_set = []
     index = 0
     for token in code:
         if('ctrl' in token):
@@ -134,8 +212,16 @@ def generate_codes(code_type, max_nb_actions):
             code[index] = current_ctrl_set[0]
             open_set.append(current_ctrl_set[1])
             close_set.append(current_ctrl_set[2])
+            cond_set.append(current_ctrl_set[3])
         elif('copen' in token):
             code[index] = open_set[int(token[-1])-1]
+        elif('cond' in token):
+            if(cond_set[int(token[-1])-1] == 0):
+                code[index] = [str(cond[-1][0]) + str(random.choice(max_repeat))][0]
+            else:
+                temp = random.choice(cond[:-1])
+                for i in reversed(temp):
+                    code.insert(index+1,i)
         elif('cclose' in token):
             code[index] = close_set[int(token[-1])-1]
         index+=1
@@ -145,6 +231,11 @@ def generate_codes(code_type, max_nb_actions):
     for token in code:
         if('cif' in token):
             code[index]  = command_if_else[0][0]
+            
+        elif('c_cndif' in token):
+            temp = random.choice(cond[:-1])
+            for i in reversed(temp):
+                code.insert(index+1,i)
         elif('celse' in token):
             code[index]  = command_if_else[1][0]
         elif('c_ifopen' in token):
@@ -161,7 +252,7 @@ def generate_codes(code_type, max_nb_actions):
     index = 0
     ret_code = []
     while(index != len(code)):
-        if (not('action' in code[index])):
+        if (not(('action' in code[index]) or ('cond' in code[index]) or ('c_cndif' in code[index]))):
              ret_code.append(code[index])
         index +=1
 
@@ -171,7 +262,7 @@ def generate_codes(code_type, max_nb_actions):
 
 
 #Main code
-n_domains = 8
+n_domains = 24
 top_k = 10
 final_codes = []
 simulator = Simulator()
@@ -180,18 +271,20 @@ for i in range(0, (n_domains*top_k)):
     for code_type in required_ctypes: 
         for nb_actions in max_actions:
             random_code = generate_codes(code_type, nb_actions)
-            print(random_code)
-            print(nb_actions)
             parse_success, _ = simulator.get_prog_ast(random_code)
-            print("parse_success", parse_success)
             if(parse_success):
                 final_codes.append(random_code)
-            
-            
-numb_unique = len(set(map(tuple, final_codes)))        
+                text += str(random_code)  + "\n"
+numb_unique = len(set(map(tuple, final_codes)))   
+numb_feat = len(final_codes)
+total_generated = n_domains * top_k * len(max_actions)*(len(required_ctypes))
+with open(text_path, 'w') as f:
+    f.write(text)
 print("numb_unique", numb_unique)
-print("total_generated", (n_domains * top_k * len(max_actions)*(len(required_ctypes))))
-print("percentage unique", (100*numb_unique/ (n_domains * top_k * len(max_actions)*(len(required_ctypes)))))
+print("numb_feat", numb_feat)
+print("total_generated", total_generated)
+print("percentage unique", (100*numb_unique/ total_generated))
+print("percentage feature", (100*numb_feat/ total_generated))
 
                
                 
